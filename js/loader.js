6845dd84f0ea3a14a8f2b642e5dbe6fefb381a31
Juan Murillo <jg.murillo10@uniandes.edu.co>
2017-06-11 00:12:49 -0500

    loader added

---
+++ js/loader.js
@@ -0,0 +1,15 @@
+let myVar;
+let result;
+// function loaded() {
+//   myVar= setTimeout(showPage, 2500);
+// }
+
+function loaded(){
+
+  document.getElementById("loader").style.display = "none";
+  document.getElementById("content-loaded").style.display = "block";
+
+  AOS.init({
+    duration: 1200
+  });
+}
