930acf365d8da2df3bdbb6830f2dbbe4770d8005
Juan Murillo <jg.murillo10@uniandes.edu.co>
2017-06-10 22:10:51 -0500

    new template

--- assets/js/navbar.js
+++
@@ -1,9 +0,0 @@
-// $(document).scroll(function(e){
-//     var scrollTop = $(document).scrollTop();
-//     if(scrollTop > 0){
-//         console.log(scrollTop);
-//         $('.navbar').removeClass('navbar-static-top').addClass('navbar-fixed-top');
-//     } else {
-//         $('.navbar').removeClass('navbar-fixed-top').addClass('navbar-static-top');
-//     }
-// });
