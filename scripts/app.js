5ec19a70d7cb6a3a0459848087177a5281c2d60d
Juan Murillo <jg.murillo10@uniandes.edu.co>
2017-11-20 16:34:35 -0500

    update styles

--- scripts/app.js
+++ scripts/app.js
@@ -2,15 +2,21 @@
   var desktopSize = window.innerWidth > 980;
 
   var appContainer = document.getElementById('app-container');
-  var pageLoader   = document.getElementById('page-loader'); var pageLoaderLoader = document.getElementById('page-loader-loader');pageLoaderContent = document.getElementById('page-loader-content');
+  var pageLoader   = document.getElementById('page-loader');
+  var pageLoaderLoader = document.getElementById('page-loader-loader');
+  var pageLoaderContent = document.getElementById('page-loader-content');
 
-  var animationDuration = 800; pageLoaderContent.style.display = "none"
-
+  var animationDuration = 800;
+  pageLoaderContent.style.display = "none";
   setTimeout(function() {
     if (desktopSize) {
-      pageLoader.classList.add('stop'); pageLoaderContent.style.display = "block";pageLoaderLoader.style.display = "none";
+      pageLoader.classList.add('stop'); 
+      pageLoaderContent.style.display = "block";
+      pageLoaderLoader.style.display = "none";
     } else {
-      scroll(animationDuration);pageLoaderContent.style.display = "block";pageLoaderLoader.style.display = "none";
+      // scroll(animationDuration);
+      pageLoaderContent.style.display = "block";
+      pageLoaderLoader.style.display = "none";
     }
   }, animationDuration);
 
