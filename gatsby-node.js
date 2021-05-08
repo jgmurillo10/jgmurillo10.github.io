83f0a40d2dc7577f7f638731815bba2835e09cbe
Juan Guillermo Murillo Castillo <jg.murillo10@uniandes.edu.co>
2021-05-07 21:47:53 -0500

    [JuanMurillo] :: [Clean] | Deploy gh-pages
    Changes:
    - Remove /docs folder
    - Remove scripts to make build on /docs
    - Add gh-pages to handle deploy

--- gatsby-node.js
+++ gatsby-node.js
@@ -10,23 +10,8 @@ const wrapper = promise =>
     return result
   });
 
-exports.onPreInit = () => {
-  if (process.argv[2] === "build") {
-    fs.rmdirSync(path.join(__dirname, "docs"), { recursive: true })
-    fs.renameSync(
-      path.join(__dirname, "public"),
-      path.join(__dirname, "public_dev")
-    )
-  }
-}
-
 exports.onPostBuild = () => {
-  fs.renameSync(path.join(__dirname, "public"), path.join(__dirname, "docs"))
-  fs.renameSync(
-    path.join(__dirname, "public_dev"),
-    path.join(__dirname, "public")
-  )
-  fs.copyFile('CNAME', 'docs/CNAME', function (err) {
+  fs.copyFile('CNAME', 'build/CNAME', function (err) {
     if (err) throw err;
     console.log('File was copied successfully.');
   });
