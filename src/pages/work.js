3b0607361a0566723253ceb31e2c15866f429aec
Juan Guillermo Murillo <jg.murillo10@uniandes.edu.co>
2021-12-29 22:20:22 -0500

    Add private projects form buton

--- src/pages/work.js
+++ src/pages/work.js
@@ -1,124 +1,134 @@
-import React from "react";
-import PropTypes from "prop-types";
-import Helmet from "react-helmet";
-import { graphql } from "gatsby";
-import styled from "@emotion/styled";
-import Layout from "components/Layout";
-import ProjectCard from "components/ProjectCard";
+import React, { useState } from "react"
+import PropTypes from "prop-types"
+import Helmet from "react-helmet"
+import { graphql } from "gatsby"
+import styled from "@emotion/styled"
+import Layout from "components/Layout"
+import ProjectCard from "components/ProjectCard"
+import Button from "components/_ui/Button"
 
 const WorkTitle = styled("h1")`
-    margin-bottom: 1em;
+  margin-bottom: 1em;
 `
 
-const Work = ({ projects, meta }) => (
+const Work = ({ projects, meta }) => {
+  // TODO(jgmurillo10): Implement this.
+  const [logged] = useState(false)
+
+  return (
     <>
-        <Helmet
-            title={`Work | Juan Murillo`}
-            // titleTemplate={`%s | Work | Juan Murillo`}
-            meta={[
-                {
-                    name: `description`,
-                    content: meta.description,
-                },
-                {
-                    property: `og:title`,
-                    content: `Work | Juan Murillo`,
-                },
-                {
-                    property: `og:description`,
-                    content: meta.description,
-                },
-                {
-                    property: `og:type`,
-                    content: `website`,
-                },
-                {
-                    name: `twitter:card`,
-                    content: `summary`,
-                },
-                {
-                    name: `twitter:creator`,
-                    content: meta.author,
-                },
-                {
-                    name: `twitter:title`,
-                    content: meta.title,
-                },
-                {
-                    name: `twitter:description`,
-                    content: meta.description,
-                },
-                {
-                    property: `og:image`,
-                    content: meta.image,
-                },
-                {
-                    property: `twitter:image`,
-                    content: meta.image,
-                },
-            ].concat(meta)}
-        />
-        <Layout>
-            <WorkTitle>
-                Work
-            </WorkTitle>
-            <>
-                {projects.filter((project) => project.node.project_is_public).map((project, i) => (
-                    <ProjectCard
-                        key={i}
-                        category={project.node.project_category}
-                        title={project.node.project_title}
-                        description={project.node.project_preview_description}
-                        thumbnail={project.node.project_preview_thumbnail}
-                        uid={project.node._meta.uid}
-                    />
-                ))}
-            </>
-        </Layout>
+      <Helmet
+        title={`Work | Juan Murillo`}
+        // titleTemplate={`%s | Work | Juan Murillo`}
+        meta={[
+          {
+            name: `description`,
+            content: meta.description,
+          },
+          {
+            property: `og:title`,
+            content: `Work | Juan Murillo`,
+          },
+          {
+            property: `og:description`,
+            content: meta.description,
+          },
+          {
+            property: `og:type`,
+            content: `website`,
+          },
+          {
+            name: `twitter:card`,
+            content: `summary`,
+          },
+          {
+            name: `twitter:creator`,
+            content: meta.author,
+          },
+          {
+            name: `twitter:title`,
+            content: meta.title,
+          },
+          {
+            name: `twitter:description`,
+            content: meta.description,
+          },
+          {
+            property: `og:image`,
+            content: meta.image,
+          },
+          {
+            property: `twitter:image`,
+            content: meta.image,
+          },
+        ].concat(meta)}
+      />
+      <Layout>
+        <a
+          href="https://forms.gle/x75wbdTKjTZqpywD6"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <Button>View private projects</Button>
+        </a>
+        <WorkTitle>Work</WorkTitle>
+        <>
+          {projects
+            .filter(project => logged || project.node.project_is_public)
+            .map((project, i) => (
+              <ProjectCard
+                key={i}
+                category={project.node.project_category}
+                title={project.node.project_title}
+                description={project.node.project_preview_description}
+                thumbnail={project.node.project_preview_thumbnail}
+                uid={project.node._meta.uid}
+              />
+            ))}
+        </>
+      </Layout>
     </>
-);
+  )
+}
 
 export default ({ data }) => {
-    const projects = data.prismic.allProjects.edges;
-    const meta = data.site.siteMetadata;
-    if (!projects) return null;
+  const projects = data.prismic.allProjects.edges
+  const meta = data.site.siteMetadata
+  if (!projects) return null
 
-    return (
-        <Work projects={projects} meta={meta}/>
-    )
+  return <Work projects={projects} meta={meta} />
 }
 
 Work.propTypes = {
-    projects: PropTypes.array.isRequired,
-};
+  projects: PropTypes.array.isRequired,
+}
 
 export const query = graphql`
-    {
-        prismic {
-            allProjects {
-                edges {
-                    node {
-                        project_title
-                        project_preview_description
-                        project_preview_thumbnail
-                        project_category
-                        project_post_date
-                        project_is_public
-                        _meta {
-                            uid
-                        }
-                    }
-                }
+  {
+    prismic {
+      allProjects {
+        edges {
+          node {
+            project_title
+            project_preview_description
+            project_preview_thumbnail
+            project_category
+            project_post_date
+            project_is_public
+            _meta {
+              uid
             }
+          }
         }
-        site {
-            siteMetadata {
-                title
-                description
-                author
-                image
-            }
-        }
+      }
     }
+    site {
+      siteMetadata {
+        title
+        description
+        author
+        image
+      }
+    }
+  }
 `
-
